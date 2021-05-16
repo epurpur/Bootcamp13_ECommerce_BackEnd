94b41e6109e380150f8aeb492112bc06203accc5
Erich Purpur <epurpur@gmail.com>
2021-05-16 16:06:19 -0400

    defining models

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+// import sequelize connection from config connection
+const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// sequelize.sync creates new tables according to the schema specified in the model.
+// this schema is found in models/index.js
+// new tables are created according to the schema defined in each model file
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App now running on port ${PORT}`));
 });
+
+
